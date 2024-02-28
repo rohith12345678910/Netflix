4dbd4ba885d103a8354e5f60397ac22775e45086
rockyrohi <145173658+rockyrohi@users.noreply.github.com>
2024-02-28 14:37:24 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,21 @@
+public class test {
+	public static void main(String[]args) {
+		byte age = 24;
+		short salary = 20000;
+		int myid = 123456789;
+		long myadhar = 1234567890l;
+		char intial = 'R';
+		float height = 6f;
+		double heightcm = 164.0976d;
+		boolean inactive = true;
+		
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(myid);
+		System.out.println(myadhar);
+		System.out.println(intial);
+		System.out.println(height);
+		System.out.println(heightcm);
+		System.out.println(inactive);
+	}
+}
